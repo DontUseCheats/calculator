159e58d75d4e96cc22ddf88542efd6de8e2a48f1
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-03-29 04:08:36 -0700

    Created button functionality in js

--- script.js
+++ script.js
@@ -28,5 +28,34 @@ switch (inputedOperator) {
     case "-":
         return subtract(num1, num2);
         break;
+    case "*":
+        return multiply(num1, num2);
+        break;
+    case "/":
+        return divide(num1, num2);
+        break;
     }
 }
+let currentStage = 0;
+let displayNum1 = 0;
+let displayOperator = "";
+let displayNum2 = 0;
+
+const btn = document.querySelectorAll(".button"); 
+
+btn.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        if (currentStage === 0) {
+            displayNum1 = event.target.textContent;
+            currentStage += 1;
+        }
+        else if (currentStage === 1) {
+            displayOperator = event.target.textContent;
+            currentStage += 1;
+        }
+        else {
+            displayNum2 = event.target.textContent;
+            currentStage += 1;
+        }
+    })
+})
